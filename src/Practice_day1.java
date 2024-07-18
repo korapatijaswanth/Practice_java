c13c42d23030305a940bd917ad272779b8f9fefc
korapatijaswanth <jaswanth15299@gmail.com>
2024-07-17 20:41:31 -0500

    java

--- src/Practice_day1.java
+++ src/Practice_day1.java
@@ -1,1 +1,24 @@
 
+class bicycle{
+    protected int gear;
+    protected int speed;
+    public bicycle(int startgear,int startspeed){
+        gear = startgear:
+        speed = startspeed;
+    }
+    public void setgear(int newvalue){
+        gear = newvalue;
+    } 
+           
+            public void setgear(int decrement){
+                speed-=decrement;
+            {
+            public void speedup(int increment){
+                speed+= increment;
+            
+            } 
+        }
+    }
+
+
+    }
